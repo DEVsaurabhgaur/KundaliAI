dfd66897c92e6f19befe3fc80e9d2615e42ee6fc
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-07-07 11:03:11 +0530

    feat: add barrel export index for all custom hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,9 @@
+/** Barrel export for all custom hooks */
+export { useDebounce } from './useDebounce';
+export { useLocalStorage } from './useLocalStorage';
+export { useWindowSize } from './useWindowSize';
+export { useClipboard } from './useClipboard';
+export { useScrollToTop } from './useScrollToTop';
+export { usePrevious } from './usePrevious';
+export { useIsVisible } from './useIsVisible';
+
