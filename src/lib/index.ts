0617c8ba70345b814c1a6c08ac2eb423cdd0df47
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-07-08 21:09:31 +0530

    refactor: update lib barrel export to include all new utility modules

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,33 @@
-/** Barrel export for all KundaliAI utilities */
-export * from './colors';
-export * from './constants';
-export * from './dateUtils';
-export * from './errorMessages';
-export * from './formatters';
-export * from './logger';
-export * from './storageKeys';
-export * from './stringUtils';
-export * from './types';
-export * from './validationUtils';
+﻿/**
+ * KundaliAI Library â€” Barrel export
+ * Import from this file for tree-shakeable access to all lib utilities.
+ */
 
+export * from './types';
+export * from './constants';
+export * from './formatters';
+export * from './dateUtils';
+export * from './stringUtils';
+export * from './validationUtils';
+export * from './colors';
+export * from './storageKeys';
+export * from './logger';
+export * from './nakshatra';
+export * from './dasha';
+export * from './dignity';
+export * from './yogas';
+export * from './ayanamsha';
+export * from './houseStrength';
+export * from './aspects';
+export * from './mangalDosha';
+export * from './kaalSarp';
+export * from './panchang';
+export * from './muhurta';
+export * from './sadeSati';
+export * from './grahaMaitri';
+export * from './geocoding';
+export * from './lagna';
+export * from './planetDescriptions';
+export * from './seo';
+export * from './analytics';
+export * from './chartExport';
