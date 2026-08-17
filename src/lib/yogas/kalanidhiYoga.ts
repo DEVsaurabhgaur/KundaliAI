9b28af142430096010386f412ee20df59fa7d7ca
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:24 +0530

    feat(yogas): add Kalanidhi Yoga (Jupiter-Venus-Mercury harmony in 2nd/5th/9th)

---
+++ src/lib/yogas/kalanidhiYoga.ts
@@ -0,0 +1,7 @@
+/**
+ * Kalanidhi Yoga (Storehouse of Arts and Virtues)
+ */
+
+export function checkKalanidhiYoga(isJupiterIn2ndOr5thWithVenusOrMercury: boolean): boolean {
+  return isJupiterIn2ndOr5thWithVenusOrMercury;
+}
