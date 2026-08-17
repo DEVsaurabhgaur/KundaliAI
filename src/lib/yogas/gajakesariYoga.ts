7930d783661ddcd4436d396b4fa279f4906657ce
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:40 +0530

    feat(yogas): add Gajakesari Yoga elephant-lion royal majesty validator

---
+++ src/lib/yogas/gajakesariYoga.ts
@@ -0,0 +1,7 @@
+/**
+ * Gajakesari Yoga (Jupiter in Kendra from Moon)
+ */
+
+export function checkGajakesariYoga(houseOfJupiterFromMoon: number): boolean {
+  return [1, 4, 7, 10].includes(houseOfJupiterFromMoon);
+}
