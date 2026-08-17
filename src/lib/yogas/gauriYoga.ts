17a53fe050ed6cc291e9718b2f4c0db3ff76f2be
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:24 +0530

    feat(yogas): add Gauri Yoga (Exalted Moon and strong 9th lord)

---
+++ src/lib/yogas/gauriYoga.ts
@@ -0,0 +1,7 @@
+/**
+ * Gauri Yoga (Grace of Goddess Gauri)
+ */
+
+export function checkGauriYoga(isMoonExaltedOrInOwnSign: boolean, is9thLordStrong: boolean): boolean {
+  return isMoonExaltedOrInOwnSign && is9thLordStrong;
+}
