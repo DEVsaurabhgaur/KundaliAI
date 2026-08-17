f0f14ecfdf20611b63e145c52e186ab2f2f65d82
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:25 +0530

    feat(yogas): add Bharathi Yoga (World-renowned author and orator)

---
+++ src/lib/yogas/bharathiYoga.ts
@@ -0,0 +1,7 @@
+/**
+ * Bharathi Yoga (Master of Literature and Oratory)
+ */
+
+export function checkBharathiYoga(is2ndLordWithJupiterInKendra: boolean): boolean {
+  return is2ndLordWithJupiterInKendra;
+}
