2974e7f52c03c9425e582c42a995d9d525edbb22
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:25 +0530

    feat(yogas): add Chatussagara Yoga (Planets occupying all 4 Kendras)

---
+++ src/lib/yogas/chatussagaraYoga.ts
@@ -0,0 +1,7 @@
+/**
+ * Chatussagara Yoga (Four Oceans - Global Influence)
+ */
+
+export function checkChatussagaraYoga(occupiedKendrasCount: number): boolean {
+  return occupiedKendrasCount === 4;
+}
