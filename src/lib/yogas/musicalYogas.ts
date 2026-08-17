35faa747b724b5657032d0e0e9b66f5a7bcd39c9
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:25 +0530

    feat(yogas): add Vallaki and Veena Nabhasa Yogas for musical genius

---
+++ src/lib/yogas/musicalYogas.ts
@@ -0,0 +1,7 @@
+/**
+ * Vallaki (Veena) Yoga - 7 planets distributed across 7 signs
+ */
+
+export function checkVeenaYoga(distinctSignsCount: number): boolean {
+  return distinctSignsCount === 7;
+}
