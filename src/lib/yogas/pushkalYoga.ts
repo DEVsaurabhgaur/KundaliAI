f1d4a9b3f02ca18bf0c70e0335c789d71836e867
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:24 +0530

    feat(yogas): add Pushkal Yoga (Moon and Ascendant lord mutual reinforcement)

---
+++ src/lib/yogas/pushkalYoga.ts
@@ -0,0 +1,7 @@
+/**
+ * Pushkal Yoga (Magnetic Splendor and Public Glory)
+ */
+
+export function checkPushkalYoga(isLagnaLordWithMoonInKendra: boolean): boolean {
+  return isLagnaLordWithMoonInKendra;
+}
