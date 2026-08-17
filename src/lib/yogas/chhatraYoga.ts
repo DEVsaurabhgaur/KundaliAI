af276ab81317c56db6020352fca46f3e41b8319c
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:25 +0530

    feat(yogas): add Chhatra Yoga (Royal canopy protection and safety)

---
+++ src/lib/yogas/chhatraYoga.ts
@@ -0,0 +1,7 @@
+/**
+ * Chhatra Yoga (Royal Canopy of Protection)
+ */
+
+export function checkChhatraYoga(continuousSignsOccupiedFrom7th: boolean): boolean {
+  return continuousSignsOccupiedFrom7th;
+}
