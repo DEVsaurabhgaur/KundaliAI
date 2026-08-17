1af7c1a45ee9613f0eb97a9ee4d7207654b22754
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:24 +0530

    feat(yogas): add Vasumati Yoga (Upachaya benefic wealth configuration)

---
+++ src/lib/yogas/vasumatiYoga.ts
@@ -0,0 +1,8 @@
+/**
+ * Vasumati Yoga (Immense Self-Earned Riches)
+ * All benefics (Jupiter, Venus, Mercury) in Upachaya houses (3, 6, 10, 11).
+ */
+
+export function checkVasumatiYoga(beneficsInUpachayaCount: number): boolean {
+  return beneficsInUpachayaCount >= 2;
+}
