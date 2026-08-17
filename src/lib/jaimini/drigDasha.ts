00842c7a3bc3e4ba28fae95bff368a7ef414c033
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:19 +0530

    feat(jaimini): add Drig Dasha spiritual vision sequence for soul evolution

---
+++ src/lib/jaimini/drigDasha.ts
@@ -0,0 +1,7 @@
+/**
+ * Drig Dasha (Aspectual Dasha for Spiritual Sadhana and Jnana)
+ */
+
+export function calculateDrigDashaStart(ninthHouseSignIndex: number): number {
+  return ninthHouseSignIndex;
+}
