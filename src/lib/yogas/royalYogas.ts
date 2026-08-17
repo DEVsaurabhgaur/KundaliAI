0385e27093c171d5b1842568ad4b596d857118de
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:23 +0530

    feat(yogas): add Royal Yogas (Shankha, Bheri, Mridanga) oratorical and sovereign combinations

---
+++ src/lib/yogas/royalYogas.ts
@@ -0,0 +1,7 @@
+/**
+ * Musical & Sovereign Yogas: Shankha, Bheri, Mridanga
+ */
+
+export function checkShankhaYoga(is5thAnd6thLordsInKendra: boolean): boolean {
+  return is5thAnd6thLordsInKendra;
+}
