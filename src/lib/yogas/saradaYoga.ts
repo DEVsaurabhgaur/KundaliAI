23588e21f62ea6ec2038aba6ac72c8828a56b81f
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-17 22:28:25 +0530

    feat(yogas): add Sarada Yoga (Scholarly patron of arts and sciences)

---
+++ src/lib/yogas/saradaYoga.ts
@@ -0,0 +1,7 @@
+/**
+ * Sarada Yoga (Grace of Goddess Sarada / Saraswati)
+ */
+
+export function checkSaradaYoga(is10thLordIn5thAndSunStrong: boolean): boolean {
+  return is10thLordIn5thAndSunStrong;
+}
