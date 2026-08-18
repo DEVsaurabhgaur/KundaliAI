4d1ee9b6cf233d180fcd24b9a38a504dae9729f3
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-18 14:25:40 +0530

    feat(nadi): add Sun-Saturn Pitru-Karma father-son karmic lesson combinations

---
+++ src/lib/nadi/sunSaturnNadi.ts
@@ -0,0 +1,7 @@
+/**
+ * Sun-Saturn (Surya-Shani) Nadi Combination
+ */
+
+export function evaluateSunSaturnNadi(): string {
+  return 'Surya-Shani Nadi: Karmic discipline with ancestral authorities, gradual self-earned sovereign status.';
+}
