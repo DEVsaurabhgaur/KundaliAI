c47a3399c307aee392c4ad41537bb12bdfe75376
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-18 14:25:42 +0530

    feat(nadi): add Jupiter-Ketu Moksha and Spiritual Enlightenment axis in Nadi

---
+++ src/lib/nadi/jupiterKetuNadi.ts
@@ -0,0 +1,7 @@
+/**
+ * Jupiter-Ketu (Guru-Ketu) Nadi Combination
+ */
+
+export function evaluateJupiterKetuNadi(): string {
+  return 'Guru-Ketu Nadi: Supreme spiritual detachment, occult intuition, philosophical gnana, liberation drive.';
+}
