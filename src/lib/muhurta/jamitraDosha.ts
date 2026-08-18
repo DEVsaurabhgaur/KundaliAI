319128081520ffa742a54709a0ff00ebcc83fa8c
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-18 14:25:56 +0530

    feat(muhurta): add Jamitra Dosha 7th house affliction filter in wedding Muhurtas

---
+++ src/lib/muhurta/jamitraDosha.ts
@@ -0,0 +1,7 @@
+/**
+ * Jamitra Dosha (Malefic Occupancy in 7th House of Muhurta Lagna)
+ */
+
+export function checkJamitraDosha(isMaleficIn7thHouse: boolean): boolean {
+  return isMaleficIn7thHouse;
+}
