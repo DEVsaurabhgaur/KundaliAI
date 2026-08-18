34e49a2ca8863d192a3da6bccc208c09451e50b7
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-18 14:25:38 +0530

    feat(muhurta): add Commercial Muhurtas (Griha Pravesh, Vyapar Arambh, Vahan Kraya)

---
+++ src/lib/muhurta/commercialMuhurtas.ts
@@ -0,0 +1,7 @@
+/**
+ * Commercial Muhurtas: Griha Pravesh, Vyapar Arambh, Vahan Kraya
+ */
+
+export function checkGrihaPraveshEligibility(isUttarayan: boolean, isTaraShuddhi: boolean): boolean {
+  return isUttarayan && isTaraShuddhi;
+}
