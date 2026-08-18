cb87b1d27521ac5795f98ba1caa2e895df5f593b
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-18 14:25:41 +0530

    feat(tajika): add Saham Lord dispositor strength and transit aspect trigger

---
+++ src/lib/tajika/sahamLordSignificators.ts
@@ -0,0 +1,7 @@
+/**
+ * Saham Lord & Trigger Engine
+ */
+
+export function evaluateSahamFruition(isSahamLordBenefic: boolean, hasBeneficAspect: boolean): boolean {
+  return isSahamLordBenefic || hasBeneficAspect;
+}
