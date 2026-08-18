75b064c8157064692776ca536daebabc492c35e2
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-18 14:25:56 +0530

    feat(chara): add Amatyakaraka (AmK) career inflection point tracking in Chara Dasha

---
+++ src/lib/chara/charaAmatyakarakaCareer.ts
@@ -0,0 +1,7 @@
+/**
+ * Amatyakaraka (AmK) Career Milestones in Chara Dasha
+ */
+
+export function isAmKDashaActive(activeDashaSign: number, amkSign: number): boolean {
+  return activeDashaSign === amkSign;
+}
