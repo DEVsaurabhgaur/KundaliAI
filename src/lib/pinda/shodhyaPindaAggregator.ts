41f745d9b52f71885154c2ec74c038681d50d474
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-19 21:26:18 +0530

    feat(pinda): add Shodhya Pinda (Rashi + Graha) aggregator for all 7 planets

---
+++ src/lib/pinda/shodhyaPindaAggregator.ts
@@ -0,0 +1,7 @@
+/**
+ * Shodhya Pinda = Rashi Pinda + Graha Pinda
+ */
+
+export function calculateShodhyaPinda(rashiPinda: number, grahaPinda: number): number {
+  return rashiPinda + grahaPinda;
+}
