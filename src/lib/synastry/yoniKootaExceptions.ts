4b177c12bc2c34682411aec1336dbacf2fbdd666
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-19 21:26:21 +0530

    feat(synastry): add Yoni Koota sworn enemy mitigating factors in synastry

---
+++ src/lib/synastry/yoniKootaExceptions.ts
@@ -0,0 +1,7 @@
+/**
+ * Yoni Koota Sworn Enemy Cancellation
+ */
+
+export function isYoniDoshaMitigated(hasFullBhakoot: boolean, hasFullNadi: boolean): boolean {
+  return hasFullBhakoot && hasFullNadi;
+}
