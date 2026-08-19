a87706dd36da08e15ed3564c679ebf60be33e3f4
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-19 21:26:22 +0530

    feat(medical): add Maraka houses (2nd and 7th) vital inflection timing

---
+++ src/lib/medical/marakaTiming.ts
@@ -0,0 +1,7 @@
+/**
+ * Maraka Houses (2nd & 7th) and Killers of Vital Energy
+ */
+
+export function isMarakaLord(houseLordNumber: number): boolean {
+  return houseLordNumber === 2 || houseLordNumber === 7;
+}
