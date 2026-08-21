56a18481aa3e6cd476175e2a2490ca4fbe565699
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-21 11:15:00 +0530

    feat(nimitta): add Duta Lakshana (Messenger demeanor, attire, first syllable) signs

---
+++ src/lib/nimitta/dutaLakshana.ts
@@ -0,0 +1,7 @@
+/**
+ * Duta Lakshana (Messenger / Querent Demeanor Analysis in Nimitta)
+ */
+
+export function evaluateDutaPosture(isFacingEastOrNorth: boolean, isHoldingFlowersOrFruit: boolean): boolean {
+  return isFacingEastOrNorth && isHoldingFlowersOrFruit;
+}
