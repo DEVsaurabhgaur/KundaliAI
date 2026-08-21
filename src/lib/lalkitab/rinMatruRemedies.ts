e6d3a0265659707f309fedf9a636bda10f895827
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-21 14:45:00 +0530

    feat(lalkitab): add Lal Kitab Matru Rin (Maternal Debt) identification and silver offerings

---
+++ src/lib/lalkitab/rinMatruRemedies.ts
@@ -0,0 +1,6 @@
+export function evaluateMatruRin(is4thAfflictedByKetuOrSaturn: boolean) {
+  return {
+    hasMatruRin: is4thAfflictedByKetuOrSaturn,
+    remedy: is4thAfflictedByKetuOrSaturn ? 'Collect equal silver pieces from blood relatives and immerse in holy river.' : 'No active Matru Rin.'
+  };
+}
