53f5c56c797a4bf29c2a5240312037dcefdef73c
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-22 11:18:15 +0530

    feat(kalasarpa): add Shankhachuda Kala Sarpa (9th-3rd axis) father dynamics & philosophical renown

---
+++ src/lib/kalasarpa/shankhachudaDharmaTriumph.ts
@@ -0,0 +1,3 @@
+export function evaluateShankhachudaImpact(rahuHouse: number): string {
+  return rahuHouse === 9 ? 'Shankhachuda Kala Sarpa: Philosophical eminence and international academic acclaim.' : 'Non-Shankhachuda Alignment.';
+}
