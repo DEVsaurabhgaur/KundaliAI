a35326ac44b771e7916fa43583e67583fa7e44be
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-22 11:18:24 +0530

    perf(kalasarpa): add modular barrel exports for Kala Sarpa & Naga Dosha engine

---
+++ src/lib/kalasarpa/index.ts
@@ -0,0 +1,6 @@
+export * from './kalaSarpa12Types';
+export * from './kalaSarpaSavya';
+export * from './kalaSarpaCancellations';
+export * from './nagaDosha';
+export * from './partialKalaSarpa';
+export * from './sarpaShantiMantras';
