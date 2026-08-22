9117021b851f5c584b0488f0b640ee4bc12623c3
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-22 11:18:16 +0530

    feat(medini): add Solar Eclipse (Surya Grahana) regime transformation indicators

---
+++ src/lib/medini/suryaGrahanaPolitical.ts
@@ -0,0 +1,3 @@
+export function evaluateSolarEclipseRegime(isTotalEclipse: boolean): string {
+  return isTotalEclipse ? 'Purna Surya Grahana: Significant sovereign diplomatic realignment in path of totality.' : 'Partial Solar Ingress Adjustment.';
+}
