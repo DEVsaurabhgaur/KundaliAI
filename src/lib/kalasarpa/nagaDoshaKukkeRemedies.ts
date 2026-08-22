4ff6724febdaf978251c9d02eb22e00a95fcc38b
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-22 11:18:17 +0530

    feat(kalasarpa): add Subramanya Kukke & Kalahasti sacred pilgrimage remedy specifications

---
+++ src/lib/kalasarpa/nagaDoshaKukkeRemedies.ts
@@ -0,0 +1,9 @@
+export const NAGA_PILGRIMAGES = [
+  'Kukke Sri Subramanya Temple (Karnataka) - Ashlesha Bali & Sarpa Samskara',
+  'Sri Kalahasti Temple (Andhra Pradesh) - Rahu-Ketu Kshetra Nivarana',
+  'Thirunageswaram (Tamil Nadu) - Milk Abhisheka to Rahu Bhagavan'
+];
+
+export function getNagaRemedyPilgrimages(): string[] {
+  return NAGA_PILGRIMAGES;
+}
