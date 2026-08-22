ce23ca5a748dcb55d96caf538704312df55e9273
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-22 11:18:24 +0530

    perf(drekkana): add modular barrel exports for Drekkana 36 faces & Kharesh engine

---
+++ src/lib/drekkana/index.ts
@@ -0,0 +1,5 @@
+export * from './drekkana36Faces';
+export * from './kharesh22nd';
+export * from './trimshamshaPathology';
+export * from './drekkanaLagnaVigor';
+export * from './drekkanaVargaChartBuilder';
