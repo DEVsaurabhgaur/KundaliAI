080cd0f4962692dda418a31cc8f529f89c71bcf1
Saurabh Gaur <saurabhgaur122000@gmail.com>
2026-08-22 11:18:17 +0530

    feat(speed): add Accelerated planet (Atisheeghra) rapid event delivery timing

---
+++ src/lib/speed/fastPlanetTransitImpact.ts
@@ -0,0 +1,3 @@
+export function evaluateAtisheeghraImpact(isAtisheeghra: boolean): string {
+  return isAtisheeghra ? 'Atisheeghra Velocity: Accelerated manifestation of dasha results within condensed timeframes.' : 'Standard Transit Cadence.';
+}
